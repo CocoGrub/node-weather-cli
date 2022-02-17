71b15db3179b9554a8010c5dc1a007809c7e0bca
Tedd <fedsavin@gmail.com>
2022-02-17 17:09:41 +0300

    axios request

--- services/api.service.js
+++ services/api.service.js
@@ -1,32 +1,25 @@
 import https from "https";
-import { TOKEN_DICTIONARY,getKey } from "./storage.service.js";
+import { TOKEN_DICTIONARY, getKey } from "./storage.service.js";
+import axios from "axios";
 
 const getWeather = async (city) => {
   const token = await getKey();
   if (!token) {
     return false;
   }
-  const data = "";
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-  url.searchParams.append("q", city);
-  url.searchParams.append("appid", token);
-  url.searchParams.append("lang", "ru");
-  url.searchParams.append("units", "metric");
 
-  https.get(url, (response) => {
-    let res = "";
-    response.on("data", (chunk) => {
-      res += chunk;
-    });
-
-    response.on("end", () => {
-      console.log(res);
-    });
-
-    response.on("error", (err) => {
-      console.log(err);
-    });
-  });
+  const {data} = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      params: {
+        q: city,
+        appid: token,
+        lang: "ru",
+        units: "metric",
+      },
+    }
+  );
+    console.log(data);
 
   return data;
 };
